df3f3b0c3de28092a8fa03bbcdeba5f0c8245561
edwinsiby <edwinsibyrajakumary@gmail.com>
2023-08-09 15:13:44 +0530

    test

--- pkg/db/database.go
+++ pkg/db/database.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 	"service3/pkg/entity"
+	"service3/pkg/utils"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -11,8 +12,8 @@ import (
 var DB *gorm.DB
 
 func ConnectToDB() (*gorm.DB, error) {
-	// config, err := utils.LoadConfig("./")
-	db, err := gorm.Open(postgres.Open("host=db user=edwin dbname=edwin password=acid port=5432 sslmode=disable"), &gorm.Config{})
+	config, err := utils.LoadConfig("./")
+	db, err := gorm.Open(postgres.Open(config.DSN), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
